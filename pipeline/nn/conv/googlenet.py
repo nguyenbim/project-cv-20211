4cddf807ab7467ef0dbb79a68ba1b12f42f61d24
nguyenbim <51375333+nguyenbim@users.noreply.github.com>
2022-01-11 13:00:31 +0700

    update import

--- pipeline/nn/conv/googlenet.py
+++ pipeline/nn/conv/googlenet.py
@@ -1,18 +1,18 @@
 # import packages
-from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
-from keras.layers.convolutional import AveragePooling2D
-from keras.layers.core import Dense
-from keras.layers.core import Activation
-from keras.layers.advanced_activations import ELU
-from keras.layers.core import Dropout
-from keras.layers import Flatten
-from keras.layers import Input
-from keras.layers import concatenate
-from keras.models import Model
-from keras.regularizers import l2
-from keras import backend as K
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import ELU
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras import backend as K
 
 class GoogLeNet:
     @staticmethod
